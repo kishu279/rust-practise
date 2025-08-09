ea76f12e655efcda23c971f01079dd9ee8d23c35
kishu279 <souravpoddar6677@gmail.com>
2025-08-09 10:40:35 +0530

    upto strings

--- rustlings/exercises/09_strings/strings3.rs
+++ rustlings/exercises/09_strings/strings3.rs
@@ -1,13 +1,20 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let mut value = input.to_string();
+    value.push_str(" world!");
+
+    value
 }
+
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
