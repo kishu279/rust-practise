8196a75a69592888dd50d7de8773cbf124d18a84
kishu279 <souravpoddar6677@gmail.com>
2025-08-14 13:07:59 +0530

    added the generics

--- assignments/assignment4/src/main.rs
+++ assignments/assignment4/src/main.rs
@@ -1,14 +1,39 @@
-use std::{iter, vec};
-
-
+use std::cmp::PartialOrd;
+use std::{vec};
 
 fn main() {
     println!("Hello, world!");
 
     let my_vec = vec![1, 2, 3, 4];
-    
-    let v: Vec<i32> = my_vec.iter().filter(|p| *p % 2 == 0).map(|f| *f * 2).collect();
 
-    println!("{:?}", v);
+    let new_vec: Vec<i32> = my_vec.iter().filter(|p| *p % 2 == 0).map(|f| f * 2).collect();
+
+    println!("{:?}", new_vec);
+
+    let ans = largest(&my_vec);
+    println!("{ans}");
 }
 
+// fn largest<T: PartialOrd>(list: &[T]) -> &T {   // partialord is used for comparision related issue when unknown types
+//     let mut largest = &list[0];
+
+//     for i in list {
+//         if largest < i {
+//             largest = i;
+//         }
+//     }
+
+//     largest
+// }
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T { // copy trait will used to pass the value not the reference
+    let mut largest = list[0];
+
+    for i in list {
+        if largest < *i {
+            largest = *i;
+        }
+    }
+
+    largest
+}
