ffd2d596533b6f7e7a1f17a8664bf95a31e4e11c
kishu279 <souravpoddar6677@gmail.com>
2025-08-15 10:12:20 +0530

    traits

--- assignments/assignment4/src/main.rs
+++ assignments/assignment4/src/main.rs
@@ -1,6 +1,21 @@
 use std::cmp::PartialOrd;
 use std::{vec};
 
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+struct User {
+    name: String,
+    age: i32,
+}
+
+impl Summary for User {
+    fn summarize(&self) -> String {
+        return format!("Name is {} and age is {}", self.name, self.age);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 
@@ -12,6 +27,15 @@ fn main() {
 
     let ans = largest(&my_vec);
     println!("{ans}");
+
+
+    let my_user = User {
+        name: String::from("Sourav Poddar"),
+        age: 20
+    };
+
+    println!("{}", my_user.summarize());
+
 }
 
 // fn largest<T: PartialOrd>(list: &[T]) -> &T {   // partialord is used for comparision related issue when unknown types
@@ -37,3 +61,4 @@ fn largest<T: PartialOrd + Copy>(list: &[T]) -> T { // copy trait will used to p
 
     largest
 }
+
